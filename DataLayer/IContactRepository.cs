a2ea6e435b07d7daf5f1156537f30a6a24fde9e7
mohammad javad tavakoli <m.j.tavakoly66@gmail.com>
2022-01-15 00:53:58 +0330

    Dapper

---
+++ DataLayer/IContactRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace DataLayer
+{
+    public interface IContactRepository
+    {
+        
+    }
+}
---
+++ Runner/Extensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Runner
+{
+    public class Extensions
+    {
+        
+    }
+}
